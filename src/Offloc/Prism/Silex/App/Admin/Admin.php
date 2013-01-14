17399e2224a536b5ed7af0b3341af41637c60417
Beau Simensen <beau@dflydev.com>
2013-01-13 20:47:05 -0800

    Placeholder.

---
+++ src/Offloc/Prism/Silex/App/Admin/Admin.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is a part of offloc/prism-silex.
+ *
+ * (c) Offloc Incorporated
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Offloc\Prism\Silex\App\Admin;
+
+class Admin
+{
+}
